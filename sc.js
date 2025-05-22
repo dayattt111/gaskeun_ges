3a94cd5695eac7ebd5493f80a7e4c454294b1a74
dayattt111 <awdayaa1@gmail.com>
2025-05-22 12:19:10 +0800

    asasa

---
+++ sc.js
@@ -0,0 +1,13 @@
+const photos = document.querySelectorAll('.photo');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+}, {
+  threshold: 0.2,
+});
+
+photos.forEach(photo => observer.observe(photo));
